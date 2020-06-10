3b12ae7e295c0c1174af9b2084e150c77a003fa4
asdf-codes <oo@noadjective.com>
2020-06-10 16:11:30 -0400

    had to restart the project completely, luckly i was able to just copy and paste everythign

---
+++ src/pages/index.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+import Layout from '../components/Layout'
+import Newsletter from '../components/newsletter'
+
+export default function Home() {
+  return (
+    <>
+    <Layout>
+      <Head>
+        
+        <link rel="icon" href="/icon.png" />
+      </Head>
+        
+        <Newsletter/>
+    </Layout>
+    </>
+  )
+}
